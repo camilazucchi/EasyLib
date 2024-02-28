f0b7ebb18dc4871a0502e207753c4158a6be450d
camilazci <camilazucchi@outlook.com>
2024-02-28 15:16:14 -0300

    refactor: remove borrowed attribute and add toString method

--- src/model/entities/Book.java
+++ src/model/entities/Book.java
@@ -8,44 +8,27 @@ public class Book {
     private String title;
     private String author;
     private Year year;
-    private boolean borrowed;
 
-    public Book(String title, String author, Year year, boolean borrowed) {
+    public Book(String title, String author, Year year) {
         this.title = title;
         this.author = author;
         this.year = year;
-        this.borrowed = borrowed;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 
     public Year getYear() {
         return year;
     }
 
-    public void setYear(Year year) {
-        this.year = year;
-    }
-
-    public boolean isBorrowed() {
-        return borrowed;
-    }
-
-    public void setBorrowed(boolean borrowed) {
-        this.borrowed = borrowed;
+    @Override
+    public String toString() {
+        return "Title: " + getTitle() + "\nAuthor: " + getAuthor() + "\nYear: " + getYear();
     }
 }
