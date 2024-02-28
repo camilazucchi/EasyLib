8ff9d7c7ae7df2ca8dd272e43874d75c9ea459cf
camilazci <camilazucchi@outlook.com>
2024-02-28 15:16:51 -0300

    refactor: remove borrowed method and add a method to remove a book

--- src/model/entities/Library.java
+++ src/model/entities/Library.java
@@ -9,31 +9,23 @@ public class Library {
     // Cria uma lista de livros do tipo Book.
     private List<Book> books = new ArrayList<>();
 
-    public Library(List<Book> books) {
-        this.books = books;
-    }
-
     // Adiciona um livro na lista:
     public void addNewBook(Book book) {
         books.add(book);
     }
 
-    // Empresta um livro:
-    public void borrowBook(Book book) {
-        // TODO: Verificar se o livro já foi emprestado.
-        book.setBorrowed(true);
-    }
-
-    // Retorna um livro que foi emprestado:
-    public void returnBook(Book book) {
-        // TODO: Verificar se o livro foi emprestado ou não antes de devolver.
-        book.setBorrowed(false);
+    public void removeBook(Book book, String name) {
+        if (name.equalsIgnoreCase(book.getTitle())) {
+            books.remove(book);
+        } else {
+            System.out.println("Sorry, the book you entered is not in your list.");
+        }
     }
 
     // Lista todos os livros:
     public void listBooks() {
         for (Book book : books) {
-            System.out.println(books);
+            System.out.println(book);
         }
     }
 
