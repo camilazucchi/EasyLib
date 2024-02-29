3f0ac431ba952124842c47c53b0c904134cd391e
camilazci <camilazucchi@outlook.com>
2024-02-29 16:28:37 -0300

    refactor: change book entity class to record

--- src/model/entities/Book.java
+++ src/model/entities/Book.java
@@ -3,32 +3,10 @@ package model.entities;
 import java.time.Year;
 
 /* Essa classe representa a entidade livro (book). */
-public class Book {
-
-    private String title;
-    private String author;
-    private Year year;
-
-    public Book(String title, String author, Year year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public Year getYear() {
-        return year;
-    }
+public record Book(String title, String author, Year year) {
 
     @Override
     public String toString() {
-        return "Title: " + getTitle() + "\nAuthor: " + getAuthor() + "\nYear: " + getYear();
+        return "Title: " + title() + "\nAuthor: " + author() + "\nYear: " + year();
     }
 }
